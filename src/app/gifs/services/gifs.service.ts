a25788b5209e835ea8b2f163c224aaed7cbc5105
Adrian Portuguez <designeradrian@gmail.com>
2024-11-12 14:20:06 -0600

    lesson 99

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = []
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'hzPnI5TQ72R3jJ7wl3ssYiPerxaxXfK8';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -22,10 +30,28 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice(0, 10)
   }
 
-  public searchTag(tag: string): void {
+  searchTag(tag: string): void {
     if (tag.length === 0) return;
-
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe(resp => {
+
+        this.gifList = resp.data
+
+      })
+
+    // or use async in searchTag and
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=hzPnI5TQ72R3jJ7wl3ssYiPerxaxXfK8&q=valorant&limit=10')
+    //   .then(resp => resp.json())
+    //   .then(data => console.log(data))
+
+
 
   }
 
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -18,4 +18,8 @@ export class SidebarComponent {
     return this.gifsService.tagsHistory;
   }
 
+  searchTag(tag: string): void {
+    this.gifsService.searchTag(tag)
+  }
+
 }
